7dcb8a3e1c88142409cc2c0a1d1ba4f208ceccc9
sfk <example@example.com>
2024-04-02 20:51:33 +0200

    just a backup

--- app/src/main/java/koziol/mooo/com/mkb2/ui/ClimbsUi.kt
+++ app/src/main/java/koziol/mooo/com/mkb2/ui/ClimbsUi.kt
@@ -4,10 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedIconButton
--- app/src/main/java/koziol/mooo/com/mkb2/ui/MainSurface.kt
+++ app/src/main/java/koziol/mooo/com/mkb2/ui/MainSurface.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
